7ffaab174f5feff4024aeeeddec4bd2480ea786e
Pedro García <pgnovato@gmail.com>
2022-04-28 19:50:19 -0500

    DB Config Completed and Basic Router-controller structure defined

---
+++ src/config/config.ts
@@ -0,0 +1,56 @@
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(__dirname, "../config/config.env") });
+
+//  Declare both interfaces to use them only in this file
+interface ENV {
+    NODE_ENV: string | undefined;
+    PORT: number | undefined;
+    DB_HOST: string | undefined;
+    DB_DATABASE: string | undefined;
+    DB_USERNAME: string | undefined;
+    DB_PASSWORD: string | undefined;
+}
+
+interface Config {
+    NODE_ENV: string;
+    PORT: number;
+    DB_HOST: string;
+    DB_DATABASE: string;
+    DB_USERNAME: string;
+    DB_PASSWORD: string;
+}
+
+//  Get config only can return data of ENV (interface) type
+const getConfig = (): ENV => {
+    return {
+        NODE_ENV: process.env.NODE_ENV,
+        PORT: process.env.PORT ? Number(process.env.PORT) : undefined,
+        DB_HOST: process.env.DB_HOST,
+        DB_DATABASE: process.env.DB_DATABASE,
+        DB_USERNAME: process.env.DB_USERNAME,
+        DB_PASSWORD: process.env.DB_PASSWORD,
+    };
+};
+
+/*
+    If some variable is not defined in the .env file, this file is
+    going to show an error in the application
+    this ensure that all the env variables are declared
+*/
+const getSanitzedConfig = (config: ENV): Config => {
+    console.log(process.env.NODE_ENV)
+    for (const [key, value] of Object.entries(config)) {
+        if (value === undefined) {
+            throw new Error(`Missing key ${key} in config.env`);
+        }
+    }
+    return config as Config;
+};
+
+const config = getConfig();
+
+const sanitizedConfig = getSanitzedConfig(config);
+
+export default sanitizedConfig;
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,1 +1,12 @@
-//  TODO: DBConnection 
+import { Sequelize } from 'sequelize';
+import config from '../config/config';
+
+const db = new Sequelize(
+    config.DB_DATABASE,
+    config.DB_USERNAME,
+    config.DB_PASSWORD, {
+        host: config.DB_HOST,
+        dialect: 'mysql',
+});
+
+export default db;
